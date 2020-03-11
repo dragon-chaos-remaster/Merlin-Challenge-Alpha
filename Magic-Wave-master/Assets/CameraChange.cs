deffe94b7c3367183dff4093052d65160d37b570
McKinley-TheProProgrammer <54154558+McKinley-TheProProgrammer@users.noreply.github.com>
2020-03-10 23:24:29 -0300

    UPDATE NIGHT 10 03 '
    - Pause Fixed
    - Changing Area Implemented
    - Movement Upgraded and Fixed
    - Caveira buffed

---
+++ Magic-Wave-master/Assets/CameraChange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    [SerializeField] Animator camerasAnimator;
+
+    //[SerializeField] bool trocarIlha = true;
+
+    [SerializeField] int nIlha;
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            camerasAnimator.SetInteger("nIlha", nIlha);
+
+            //camerasAnimator.SetBool("trocarDeIlha", trocarIlha);
+            //StartCoroutine(TempoPraTrocar());
+        }
+
+    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.tag == "player")
+    //    {
+    //        camerasAnimator.SetInteger("nIlha", 0);
+    //    }
+    //}
+
+    //IEnumerator TempoPraTrocar()
+    //{
+    //    yield return new WaitForSeconds(1f);
+    //    trocarIlha = false;
+
+    //}
+
+}
